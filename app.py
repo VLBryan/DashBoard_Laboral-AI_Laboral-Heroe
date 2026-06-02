c9d4c505e6f296468cfb1181aade9de37e8ad919
VLBryan <vlbryan@outlook.es>
2026-06-02 17:12:26 -0500

    Se crean credenciales privadas y .gitignore

--- app.py
+++ app.py
@@ -25,9 +25,17 @@ from viz_factory import (
 # -------------------------
 st.set_page_config(layout="wide", page_title="Laboral.AI - Dashboard Postulantes")
 
-DB_NAME = os.getenv("LABORAL_DB", "nombre_de_tu_db")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-CACHE_DIR = "./cache"
+# importar configuración privada
+try:
+    import config_private
+    DB_NAME = config_private.DB_NAME
+    MONGO_URI = config_private.MONGO_URI
+    CACHE_DIR = config_private.CACHE_DIR
+except ImportError:
+    # fallback si no existe el archivo (ej. en despliegue público)
+    DB_NAME = os.getenv("LABORAL_DB", "nombre_de_tu_db")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    CACHE_DIR = "./cache"
 
 # -------------------------
 # Data loading (cached)
